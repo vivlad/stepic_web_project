086ddbdb048b00f23a41143fbf1d80e8b5754f5a
vivlad <0x142857ff@gmail.com>
2016-03-02 21:47:46 +0200

    it work!

--- hello.py
+++ hello.py
@@ -1,21 +1,36 @@
+from urlparse import parse_qs, urlparse
+import os
+import cgi
 def app(environ, start_response):
-    """Simplest possible application object"""
-    #post_env = env.copy()
+	"""Simplest possible application object"""
+	#post_env = env.copy()
 	#post_env['QUERY_STRING'] = ''
 	#post = cgi.FieldStorage(
-    #	fp=env['wsgi.input'],
-    #	environ=post_env,
-    #	keep_blank_values=True
+	#	fp=env['wsgi.input'],
+	#	environ=post_env,
+	#	keep_blank_values=True
 	#)
 	# Returns a dictionary containing lists as values.
-    #d = parse_qs(environ['QUERY_STRING'])
-    #data = 'Hello, Worldss!\n'
-    data = environ['QUERY_STRING']
-    status = '200 OK'
-    response_headers = [
-        ('Content-type','text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
+	#d = parse_qs(environ['QUERY_STRING'])
+	#data = 'Hello, Worldss!\n'
+	#url = environ['QUERY_STRING']
+	#queryst = parse_qs(urlparse(url).query, keep_blank_values=True)
+	#data = os.environ.get("QUERY_STRING", "No Query String in url")
+	query = environ[ "QUERY_STRING" ]
+	if len( query ) == 0:
+		data = ''
+	else:
+		#data = cgi.escape( query )
+		pairs = cgi.parse_qs( query )
+		#data = 'data:\n' + cgi.escape( query ) + '\n'
+		data = ''
+		for key, value in pairs.items():
+			data = data + str(key) + '=' + str(value) + '\n'
+	status = '200 OK'
+	response_headers = [
+		('Content-type','text/plain'),
+		('Content-Length', str(len(data)))
+	]
 
-    start_response(status, response_headers)
-    return iter([data])
+	start_response(status, response_headers)
+	return iter([data])
